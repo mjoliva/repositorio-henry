f8eb4e2e3ee83b2b3342a642d68fe0e3049caefe
Maria Jesus <olivap.mariajesus@gmail.com>
2022-05-05 01:24:45 -0400

    first commit

---
+++ homework.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  // algo
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+}
+
+// No se pueden usar: 
+// parseInt
+// toString
+
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+};
