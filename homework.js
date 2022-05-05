458a0186575037e5dd10af451b6b67857676fdc7
Maria Jesus <olivap.mariajesus@gmail.com>
2022-05-05 01:31:48 -0400

    first commit

--- homework.js
+++ homework.js
@@ -1,19 +1,23 @@
 "use strict";
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-  // algo
+  const array = num.split('');
+  let finalResult = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    finalResult = finalResult + array[i] * Math.pow(2, array.length - 1 - i);
+  } 
+  return finalResult;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+  const array = [];
+  while (num > 0) { 
+    array.unshift(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return array.join('');
 }
-
-// No se pueden usar: 
-// parseInt
-// toString
-
-
 
 module.exports = {
   BinarioADecimal,
